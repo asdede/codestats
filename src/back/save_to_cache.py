0b3bde4c76d3020c981ed2521f5c49aef2d7ea84
asdede <alexi.tikkala95@gmail.com>
2024-12-03 20:27:40 +0200

    Now autoupdate works

--- src/back/save_to_cache.py
+++ src/back/save_to_cache.py
@@ -8,6 +8,7 @@ This is planned to use in docker container. Everytime api is run, it gets these,
 from github_gather import main as hub_main
 from gitlab_gather import main as lab_main
 from waka_gather import get_stats, get_all_time_data
+import time
 import json
 
 def save_to_json(data,f_name):
@@ -15,21 +16,23 @@ def save_to_json(data,f_name):
         f.write(json.dumps(data,indent=4))
 
 def main():
-    print("Getting github data")
-    hub_data = hub_main()
-    print("Getting gitlab data")
-    lab_data = lab_main()
-    print("Getting wakatime stats")
-    waka_data = get_stats()
-    waka_hours = get_all_time_data()
+    while True:
+        time.sleep(60) # 1min
+        print("Getting github data")
+        hub_data = hub_main()
+        print("Getting gitlab data")
+        lab_data = lab_main()
+        print("Getting wakatime stats")
+        waka_data = get_stats()
+        waka_hours = get_all_time_data()
 
 
-    print("Saving to json")
-    save_to_json(hub_data,'github.json')
-    save_to_json(lab_data,'gitlab.json')
-    save_to_json(waka_data,'wakatime.json')
-    save_to_json(waka_hours,'wakatime_hours.json')
-    print("Done!")
+        print("Saving to json")
+        save_to_json(hub_data,'github.json')
+        save_to_json(lab_data,'gitlab.json')
+        save_to_json(waka_data,'wakatime.json')
+        save_to_json(waka_hours,'wakatime_hours.json')
+        print("Done!")
     
 if __name__ == "__main__":
     main()
